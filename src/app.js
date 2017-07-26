6e8d8affc2d50e6d0504e516e3458cc7c9d40138
antweer <trajwani@gmail.com>
2017-07-25 20:48:01 -0500

    Refactored API key to secrets

--- src/app.js
+++ src/app.js
@@ -3,17 +3,11 @@ import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
+import firebaseSetup from '../SECRETS.js';
 
 class App extends Component {
   componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyCJ7qEcxe9eiqksf0MG7thiKySvv4xn-T0',
-      authDomain: 'auth-37ab9.firebaseapp.com',
-      databaseURL: 'https://auth-37ab9.firebaseio.com',
-      projectId: 'auth-37ab9',
-      storageBucket: 'auth-37ab9.appspot.com',
-      messagingSenderId: '3141029957'
-    })
+    firebase.initializeApp({firebaseSetup})
   }
 
   render() {
