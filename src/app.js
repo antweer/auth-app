d00ad69286a8f8f5a077e9f3ff81fe5a037e378a
antweer <trajwani@gmail.com>
2017-07-26 16:50:45 -0500

    Added authentication logic to test whether user is logged in

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 import { firebaseSetup } from '../SECRETS.js';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCJ7qEcxe9eiqksf0MG7thiKySvv4xn-T0',
@@ -15,13 +17,33 @@ class App extends Component {
       storageBucket: 'auth-37ab9.appspot.com',
       messagingSenderId: '3141029957'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
