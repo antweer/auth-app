aa6a1fcea23326a1fbad1894561a35eee40daf1d
antweer <trajwani@gmail.com>
2017-07-26 16:09:19 -0500

    Created a new common component for a Spinner to show while loading

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
-import firebaseSetup from '../SECRETS.js';
+import { firebaseSetup } from '../SECRETS.js';
 
 class App extends Component {
   componentWillMount() {
-    firebase.initializeApp({ firebaseSetup });
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCJ7qEcxe9eiqksf0MG7thiKySvv4xn-T0',
+      authDomain: 'auth-37ab9.firebaseapp.com',
+      databaseURL: 'https://auth-37ab9.firebaseio.com',
+      projectId: 'auth-37ab9',
+      storageBucket: 'auth-37ab9.appspot.com',
+      messagingSenderId: '3141029957'
+    });
   }
 
   render() {
